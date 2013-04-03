fbe677f55888e78811ef7b2364b1a5abfe9738f3
Jan Aage Lavik <jan.age.lavik@cern.ch>
2013-04-03 13:46:21 +0200

    BatchUploader: several improvements
    * Displays bibupload:batchupload in BibShed queue
    
    * Adds the size of the input based on the length of the email address
      so that it does not get cut-off
    
    * Does the conversion of textmarc to marcXML in the confirm step,
      to avoid the upload simulation from failing
    
    * Removes option --insert when textmarc selected on the web interface
      as it is not supported by textmarc2marcxml

--- modules/bibupload/lib/batchuploader_engine.py
+++ modules/bibupload/lib/batchuploader_engine.py
@@ -181,9 +181,6 @@ def metadata_upload(req, metafile=None, filetype=None, mode=None, exec_date=None
     req.send_http_header()
 
     error_codes = {'not_authorized': 1}
-    # write temporary file:
-    if filetype != 'marcxml':
-        metafile = _transform_input_to_marcxml(file_input=metafile)
 
     user_info = collect_user_info(req)
     tempfile.tempdir = CFG_TMPSHAREDDIR
@@ -201,7 +198,8 @@ def metadata_upload(req, metafile=None, filetype=None, mode=None, exec_date=None
             return (error_codes['not_authorized'], allow[1])
 
     # run upload command:
-    task_arguments = ('bibupload', user_info['nickname'], mode, "--name=" + metafilename, "--priority=" + priority)
+    task_arguments = ('bibupload', user_info['nickname'], mode,
+                      "--priority=" + priority, "-N", "batchupload")
     if exec_date:
         date = exec_date
         if exec_time:
@@ -330,7 +328,8 @@ def document_upload(req=None, folder="", matching="", mode="", exec_date="", exe
                 user = "batchupload"
             # Execute bibupload with the appropiate mode
 
-            task_arguments = ('bibupload', user, "--" + mode, "--name=" + docfile, "--priority=" + priority)
+            task_arguments = ('bibupload', user, "--" + mode,
+                              "--priority=" + priority, "-N", "batchupload")
 
             if exec_date:
                 date = '--runtime=' + "\'" + exec_date + ' ' + exec_time + "\'"
@@ -650,6 +649,7 @@ def _transform_input_to_marcxml(file_input=""):
         transform_file(filename)
     finally:
         sys.stdout = old_stdout
+
     return new_stdout.getvalue()
 
 def _log(msg, logfile="webupload.log"):
