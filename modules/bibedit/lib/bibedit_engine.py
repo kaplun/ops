da87210b33885a5ce54e0f502e1c8a14b0d4bf43
Javier Martin Montull <javier.martin.montull@cern.ch>
2013-07-10 17:28:41 +0200

    BibEdit: several improvements
    * Avoids double request changing to textmarc.
    
    * Fixes to avoid losing the controlfields when switching to text marc mode and
      then going back to table view.
    
    * If a template is available, merges import from crossref with a template
      (as defined in CFG_BIBEDIT_EXTEND_RECORD_WITH_COLLECTION_TEMPLATE).
    
    * Also modifies slightly the look&feel of new record page.
    
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- modules/bibedit/lib/bibedit_engine.py
+++ modules/bibedit/lib/bibedit_engine.py
@@ -592,7 +592,14 @@ def perform_request_record(req, request_type, recid, uid, data, ln=CFG_SITE_LANG
                     else:
                         record_add_field(record, '001',
                                          controlfield_value=str(new_recid))
+                        template_to_merge = extend_record_with_template(recstruct=record)
+                        if template_to_merge:
+                            merged_record = merge_record_with_template(record, template_to_merge)
+                            if merged_record:
+                                record = merged_record
+
                         create_cache_file(new_recid, uid, record, True)
+                        mtime = get_cache_mtime(recid, uid)
                         response['resultCode'], response['newRecID'] = 7, new_recid
         elif new_type == 'clone':
             # Clone an existing record (from the users cache).
@@ -607,6 +614,7 @@ def perform_request_record(req, request_type, recid, uid, data, ln=CFG_SITE_LANG
                 # Cache missing. Fall back to using original version.
                 record = get_bibrecord(recid)
             record_delete_field(record, '001')
+            record_delete_field(record, '005')
             record_add_field(record, '001', controlfield_value=str(new_recid))
             create_cache_file(new_recid, uid, record, True)
             response['resultCode'], response['newRecID'] = 8, new_recid
@@ -919,7 +927,7 @@ def perform_request_record(req, request_type, recid, uid, data, ln=CFG_SITE_LANG
     elif request_type == 'submittextmarc':
         # Textmarc content coming from the user
         textmarc_record = data['textmarc']
-        xml_conversion_status = get_xml_from_textmarc(recid, textmarc_record)
+        xml_conversion_status = get_xml_from_textmarc(recid, textmarc_record, uid)
 
         if xml_conversion_status['resultMsg'] == "textmarc_parsing_error":
             response.update(xml_conversion_status)
@@ -1545,7 +1553,7 @@ def perform_request_preview_record(request_type, recid, uid, data):
     if request_type == "preview":
         if data["submitMode"] == "textmarc":
             textmarc_record = data['textmarc']
-            xml_conversion_status = get_xml_from_textmarc(recid, textmarc_record)
+            xml_conversion_status = get_xml_from_textmarc(recid, textmarc_record, uid)
             if xml_conversion_status['resultMsg'] == 'textmarc_parsing_error':
                 response['resultCode'] = CFG_BIBEDIT_AJAX_RESULT_CODES_REV['textmarc_parsing_error']
                 response.update(xml_conversion_status)
@@ -1612,7 +1620,7 @@ def perform_request_get_tableview(recid, uid, data):
     """
     response = {}
     textmarc_record = data['textmarc']
-    xml_conversion_status = get_xml_from_textmarc(recid, textmarc_record)
+    xml_conversion_status = get_xml_from_textmarc(recid, textmarc_record, uid)
     response.update(xml_conversion_status)
 
     if xml_conversion_status['resultMsg'] == 'textmarc_parsing_error':
